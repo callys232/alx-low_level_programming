414357b5a3ab0b2ee33dd25438ac8021a09daf99
Callys Joseph <calebjella82@gmail.com>
2022-09-08 20:39:04 -0700

    Write a C program that prints exactly and that piece of art is useful

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,9 @@
 #include <stdlib.h>
 
 /**
- *  main - Entry point
- *
+ * main - Entry point
  * Return: Always 1 (Success)
  */
-
 int main(void)
 {
 	write(STDOUT_FILENO, "and that piece of art is useful
