0b4d70d57152496464cbd622b14528508a0bd789
Callys Joseph <calebjella82@gmail.com>
2022-09-12 05:45:36 -0700

    Learning C

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,14 +8,14 @@
  */
 int main(void)
 {
-	char ch;
-	for (ch = 'a' ; ch <= 'z' ; ch++)
+char ch;
+for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	putchar(ch);
+putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-	putchar(ch);
+putchar(ch);
 }
 putchar('\n');
 return (0);
