3394d2d7f9eb513813a4817cac291f9e2588f117
Callys Joseph <calebjella82@gmail.com>
2022-09-14 01:35:48 -0700

    learning c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,17 +3,15 @@
  * main - print putchar
  * Return :  always 0
  */
-
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+	char *sh = "main";
+
+	while (*sh);
+	{
+		_putchar(*sh);
+		sh++
+	}
 	_putchar('\n');
 
 	return (0);
