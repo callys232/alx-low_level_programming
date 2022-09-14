0a2fc55c21184cd668c8703846daa59836922cbb
Callys Joseph <calebjella82@gmail.com>
2022-09-14 01:52:06 -0700

    lerning c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,13 @@
  */
 int main(void)
 {
-	char *sh = "main";
-
-	while (*sh);
-	{
-		_putchar(*sh);
-		sh++
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
 	_putchar('\n');
 
 	return (0);
