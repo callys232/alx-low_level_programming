4a65abd41114733257aba583b3667568e7c30728
Callys Joseph <calebjella82@gmail.com>
2022-09-19 10:21:33 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,13 @@
  */
 void puts2(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	int i = 0;
+	for (; str[i] != '\0'; i++)
 	{
-		if (str[count] == '\0')
-		{
-			printf('\n');
-			break;
-		}
-		if (count % 2 == 0)
-			printf(str[count]);
-		count++;
+		if (i % 2) == 0)
+			_putchar(str[i]);
+		else
+			continue;
 	}
+	_putchar('\n');
 }
