388c54174ffa26506d3545aada1cbaf5a53d946f
Callys Joseph <calebjella82@gmail.com>
2022-09-30 08:27:57 -0700

    argvargc

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Print the name of the program
+ * @argc: Arugumen count
+ * @argv: Argument b]vector
+ *
+ * Return: alwasys 0 (success)
+ */
+int main(int agrc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
