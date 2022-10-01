7047c7000b9e2597ee8e164d6a56295e00d9ec9f
Callys Joseph <calebjella82@gmail.com>
2022-09-30 18:17:05 -0700

    lib

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - prints a strong
+ * @str: string to print
+ *
+ * Description: prints a string
+ * on Success: returns no error
+ */
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
+}
---
+++ 0x09-static_libraries/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strspn _ gets length of a prefi substring
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: nuber of bytes of s in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; accept[j]; j++)
+		{
+			if (s[i] == accept[j])
+				break;
+		}
+		if (!accept[j])
+			break;
+	}
+	return (i);
+}
